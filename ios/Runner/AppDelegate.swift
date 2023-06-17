1660ec8a54bacaf3a5efbec1729fc15ac9b77c8d
faris-backer <faris.backer@lxisoft.com>
2023-06-17 23:14:31 +0530

    added bluetooth connectivity in ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import <Flutter/Flutter.h
+import <CoreBluetooth/CoreBluetooth.h>
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,4 +12,30 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+static NSString *const kBluetoothChannel = @"bluetooth_channel";
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([call.method isEqualToString:@"enableBluetooth"]) {
+        CBCentralManager *centralManager = [[CBCentralManager alloc] initWithDelegate:nil queue:nil];
+        [centralManager enable];
+        result(@(YES));
+    } else if ([call.method isEqualToString:@"disableBluetooth"]) {
+        CBCentralManager *centralManager = [[CBCentralManager alloc] initWithDelegate:nil queue:nil];
+        [centralManager disable];
+        result(@(YES));
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+        methodChannelWithName:kBluetoothChannel
+              binaryMessenger:controller.binaryMessenger];
+    [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        [self handleMethodCall:call result:result];
+    }];
+}
+
 }
